93dec1080f459498540deae01aab6a35fbfe0bf4
ICE <ICE@DESKTOP-TQ0N9K9>
2016-06-19 15:15:15 +0800

    chufangyao model

--- models/ChufangYao.php
+++ models/ChufangYao.php
@@ -3,6 +3,7 @@
 namespace app\models;
 
 use Yii;
+use yii\base\Object;
 
 /**
  * This is the model class for table "{{%chufang_yao}}".
@@ -20,6 +21,25 @@ class ChufangYao extends \yii\db\ActiveRecord
     public static function tableName()
     {
         return '{{%chufang_yao}}';
+    }
+    
+    public function afterSave($insert, $changedAttributes)
+    {
+    	parent::afterSave($insert, $changedAttributes);
+    	if($insert){
+    		$model = new YaoLog();
+    		$model->yao = $this->yao;
+    		$model->weight = -$this->weight;
+    		$model->chufang_id = $this->chufang_id;
+    		$model->content = $this->chufang->name . date('Y-m-d', $this->chufang->sign_at) . '处方用药';
+    		$model->save();
+    	}
+    	return TRUE;
+    }
+    
+    public function getChufang()
+    {
+    	return $this->hasOne(Chufang::className(), ['id'=>'chufang_id']);
     }
 
     /**
